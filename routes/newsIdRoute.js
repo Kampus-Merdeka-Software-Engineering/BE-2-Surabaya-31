3321068d4d7ee9f2dc1fb46519949b41e659a07d
Desi M. Dama <101694357+desidama23@users.noreply.github.com>
2023-12-03 17:28:18 +0700

    Update newsIdRoute.js

--- routes/newsIdRoute.js
+++ routes/newsIdRoute.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const newsController = require('../controllers/newsIdController');
+const newsIdController = require('../controllers/newsIdController');
 
 // GET all newsId
-router.get('/', newsIdController.getAllNewsId);
+router.get('/', async (req, res) => {
+  try {
+    const result = await newsIdController.getAllNewsId();
+
+    // Kirim respons berdasarkan hasil dari controller
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
