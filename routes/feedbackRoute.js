61364825e157830c80c139b2c10a50b22cc0885b
Desi M. Dama <101694357+desidama23@users.noreply.github.com>
2023-12-03 17:26:50 +0700

    Update feedbackRoute.js

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
 
 // POST create feedback
-router.post('/', feedbackController.createFeedback);
+router.post('/', async (req, res) => {
+  try {
+    const result = await feedbackController.createFeedback(req.body);
+
+    // Kirim respons berdasarkan hasil dari controller
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
