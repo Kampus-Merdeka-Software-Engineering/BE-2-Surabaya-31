f44b5b45a72a89fa175a6cc7c6a693dc21e66610
Desi M. Dama <101694357+desidama23@users.noreply.github.com>
2023-12-03 09:50:12 +0700

    Delete routes directory

--- routes/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const newsController = require('../controllers/newsController');
-
-router.get('/:type', newsController.getAllNews);
-router.post('/', newsController.addNews);
-
-module.exports = router;
