c7a45146f37b7b6cc1dfc79cd86c2ba74304a7b3
Desi M. Dama <101694357+desidama23@users.noreply.github.com>
2023-12-03 17:27:38 +0700

    Update newsEnRoute.js

--- routes/newsEnRoute.js
+++ routes/newsEnRoute.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const newsEnController = require('../controllers/newsEnController');
 
-// GET all news
-router.get('/', newsEnController.getAllNewsEn);
+// GET all newsEn
+router.get('/', async (req, res) => {
+  try {
+    const result = await newsEnController.getAllNewsEn();
+
+    // Kirim respons berdasarkan hasil dari controller
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
