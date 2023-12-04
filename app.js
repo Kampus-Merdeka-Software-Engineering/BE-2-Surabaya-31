b8d3f4b2806246f77e8b8fd026697e22d9935ca7
Desi M. Dama <101694357+desidama23@users.noreply.github.com>
2023-12-04 13:34:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/cwu', cwuRouter)
-app.use('/feedback', feedbackRouter)
-app.use('/newsEn', newsEnRouter)
-app.use('/newsId', newsIdRouter)
+app.use('/cwu', cwuRouter);
+app.use('/feedback', feedbackRouter);
+app.use('/newsEn', newsEnRouter);
+app.use('/newsId', newsIdRouter);
 
 // Start server
 app.listen(PORT, () => {
